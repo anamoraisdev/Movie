b48df14e1b32ff0117c8d0f8ca183455c4bd1f98
ana <anamorais.dev@gmail.com>
2023-09-13 10:30:55 -0300

    pegando proxima pagina e a pagina anterior de resultados

--- src/components/searchResult.tsx
+++ src/components/searchResult.tsx
@@ -1,8 +1,28 @@
+import { useState } from "react"
 import { useAppSelector } from "../redux/hooks"
 import Card from "./card"
 
 const SearchResultView = () => {
     const movies = useAppSelector(state => state.movies.movies)
+    const pageAtual = useAppSelector(state => state.movies.pageAtual)
+    const [nextPage, setNextPage] = useState<number| undefined>()
+    const [previusPage, setPreviusPage] = useState<number| undefined>()
+
+
+    const getNextPage = () => {
+      if(pageAtual){
+        const page =  pageAtual + 1
+        setNextPage(page)
+      }
+    }
+
+    const getPreviusPage = () => {
+      if(pageAtual){
+        const page = pageAtual - 1
+        setPreviusPage(page)
+      }
+    }
+
     return (
         <div className="flex flex-wrap gap-4 w-full">
             {movies?.map((movie) => (
